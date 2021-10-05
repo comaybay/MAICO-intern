662dfeb884fc21711e053ff185c101d35cd9639d
CMB <thaichibao@gmail.com>
2021-10-05 14:44:42 +0700

    Update entry point (Program.Main)

--- Code/BillManager/Program.cs
+++ Code/BillManager/Program.cs
@@ -6,6 +6,29 @@ namespace BillManager
     {
         static void Main()
         {
+            Console.InputEncoding = System.Text.Encoding.Unicode;
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+
+            var inputFormatOptions = new FormatOptions()
+            {
+                IndentSize = 4,
+                LineSpacing = 2,
+            };
+
+            var displayFormatOptions = new FormatOptions()
+            {
+                IndentSize = 1,
+                LineSpacing = 1,
+            };
+
+            var app = new App
+            (
+                inputFormatOptions,
+                displayFormatOptions,
+                saveFileName: "danh_sach_hoa_don"
+            );
+
+            app.Run();
         }
     }
 }
