4d25b83c340795a3a4aa4b45e3eae483f6a5d782
CMB <thaichibao@gmail.com>
2021-10-06 09:28:37 +0700

    IStringConvertable interface

---
+++ Code/BillManager/IStringConvertable.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BillManager
+{
+    interface IStringConvertable
+    {
+        /// <summary>
+        /// Chuyển các property bên trong của class thành các dòng string.
+        /// </summary>
+        public IList<string> GetStringProps();
+
+        /// <summary>
+        /// Chuyển giá trị của các property bên trong của class thành 1 dòng string.
+        /// </summary>
+        public string GetPropValuesAsSingleString();
+    }
+}
